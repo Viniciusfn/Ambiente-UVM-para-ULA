f2f74f7acc11a59c001f79383f00f9e02ffbf05e
João Pedro Melquiades Gomes <joao.gomes@embedded.ufcg.edu.br>
2019-10-23 15:24:31 -0300

    Refmods (em C e em SV) provisórios

---
+++ refmod.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+extern "C" int my_ULA(int A, int B, short instru){
+	switch (instru){
+		case 0:
+			return A+B;
+		case 1:
+			if(A > B)
+				return A-B;
+			return B-A;
+		case 2:
+			return A+1;
+		case 3:
+			return B+1;
+		default:
+			return -1;
+	}
+}
---
+++ refmod.sv
@@ -0,0 +1,60 @@
+import "DPI-C" context function int my_ULA(int A, int B, short instru);
+
+class refmod extends uvm_component;
+  `uvm_component_utils(refmod)​
+
+  transaction_in tr_in;​
+  transaction_out tr_out;​
+  uvm_analysis_imp #(transaction_in, refmod) in;​
+  uvm_analysis_port #(transaction_out) out; ​
+  event begin_refmodtask, begin_record, end_record;​
+
+  function new(string name = "refmod", uvm_component parent);​
+    super.new(name, parent);​
+    in = new("in", this);​
+    out = new("out", this);​
+  endfunction​
+
+  virtual function void build_phase(uvm_phase phase);​
+    super.build_phase(phase);​
+    tr_out = transaction_out::type_id::create("tr_out", this);​
+
+  endfunction: build_phase​
+
+  virtual task run_phase(uvm_phase phase);​
+    super.run_phase(phase);​
+    fork​
+      refmod_task();​
+      record_tr();​
+    join​
+  endtask: run_phase​
+
+  task refmod_task();​
+    forever begin​
+      @begin_refmodtask;​
+      tr_out = transaction_out::type_id::create("tr_out", this);​
+      -> begin_record;​
+      tr_out.result = my_ULA(tr_in.dt_A, tr_in.dt_B, tr_in.dt_instru);
+      #10;​
+      -> end_record;​
+      out.write(tr_out);​
+    end​
+
+  endtask : refmod_task​
+
+  ​virtual function write (transaction_in t);​
+    tr_in = transaction_in#()::type_id::create("tr_in", this);​
+    tr_in.copy(t);​
+    -> begin_refmodtask;​
+  endfunction​
+
+  virtual task record_tr();​
+    forever begin​
+      @(begin_record);​
+      begin_tr(tr_out, "refmod");​
+      @(end_record);​
+      end_tr(tr_out);​
+    end​
+  endtask​
+
+endclass: refmod​
